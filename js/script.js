bbdd13de78e38b1324bebc006a71df34237ed5cc
RahulNathani49 <nathanirahul49@gmail.com>
2019-02-21 22:24:33 +0530

    toggle

---
+++ js/script.js
@@ -0,0 +1,23 @@
+      function b1() {
+            var sec = document.getElementsByClassName("box2");
+            sec[0].innerHTML = "<h1>C</h1>"
+
+            var thi = document.getElementsByClassName("box3");
+            thi[0].innerHTML = "<h1>B</h1>"
+        }
+        
+        function b2() {
+            var fir = document.getElementsByClassName("box1");
+            fir[0].innerHTML = "<h1>C</h1>"
+            var thi = document.getElementsByClassName("box3");
+            thi[0].innerHTML = "<h1>A</h1>"
+
+        }
+
+        function b3() {
+            var fir = document.getElementsByClassName("box1");
+            fir[0].innerHTML = "<h1>B</h1>"
+
+            var sec = document.getElementsByClassName("box2");
+            sec[0].innerHTML = "<h1>A</h1>"
+        }
